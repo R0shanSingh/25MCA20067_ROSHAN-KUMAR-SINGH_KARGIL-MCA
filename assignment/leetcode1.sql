87ebb2c0c84c1dca5bd7436679429e8aea3a612f
Roshan Kumar Singh <94280658+R0shanSingh@users.noreply.github.com>
2026-02-02 23:06:02 +0530

    Create leetcode1.sql

---
+++ assignment/leetcode1.sql
@@ -0,0 +1,5 @@
+SELECT user_id,MAX(time_stamp) AS last_stamp FROM Logins
+WHERE time_stamp >= '2020-01-01' AND time_stamp < '2021-01-01'
+GROUP BY user_id;
+
+-- https://leetcode.com/problems/the-latest-login-in-2020/submissions/1905779947/
